dcf7b6c322786e925d74337d81b7b0a949ea6e5e
Shahar s <cl-shaharshavit-rnd@users.noreply.github.com>
2021-11-09 11:25:16 +0200

    fix: connection duplication

--- src/DAL/connectionManager.ts
+++ src/DAL/connectionManager.ts
@@ -10,18 +10,21 @@ import { HeartbeatRepository } from './repositories/heartbeatRepository';
 @singleton()
 export class ConnectionManager {
   private connection?: Connection;
+  private connectionStatusPromise?: Promise<Connection>;
 
   public constructor(@inject(Services.LOGGER) private readonly logger: Logger, @inject(Services.CONFIG) private readonly config: IConfig) {}
 
   public async init(): Promise<void> {
     const connectionConfig = this.config.get<IDbConfig>('typeOrm');
-    this.logger.info(`connection to database ${connectionConfig.database as string} on ${connectionConfig.host as string}`);
-
+    this.logger.log('info', `connection to database ${connectionConfig.database as string} on ${connectionConfig.host as string}`);
     try {
-      this.connection = await createConnection(this.createConnectionOptions(connectionConfig));
+      if (this.connectionStatusPromise === undefined) {
+        this.connectionStatusPromise = createConnection(this.createConnectionOptions(connectionConfig));
+      }
+      this.connection = await this.connectionStatusPromise;
     } catch (err) {
       const errString = JSON.stringify(err, Object.getOwnPropertyNames(err));
-      this.logger.error(`failed to connect to database: ${errString}`);
+      this.logger.log('error', `failed to connect to database: ${errString}`);
       throw new DBConnectionError();
     }
   }
